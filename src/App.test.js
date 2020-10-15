ff188bc739303dc553a19a4d7d1ebe45ee8c50cc
Kristofer Walters <kristofer.walters@sparebank1.no>
2020-10-15 09:17:49 +0200

    Import all ffe.css

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Bestill time-headline', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const headlineElement = getByText(/bestill time hos klarsynt/i);
+  expect(headlineElement).toBeInTheDocument();
 });
